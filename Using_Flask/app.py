ea7e7a0bed1b1532fece6fb9a78aecf85ae52b67
hashbanger <prashant.brahmbhatt32@outlook.com>
2019-12-19 23:56:44 +0530

    completed forms and validation

---
+++ Using_Flask/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'b033b0296b67a3fd1cf2e19b329e8bcf'
+posts = [
+    {
+        'author':'Prashant Brahmbhatt',
+        'title': 'The Seven Sins',
+        'content': 'lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+        'date_posted':'April 20,2019'
+    },
+    {
+        'author':'Jon Doe',
+        'title': 'The life of a writer',
+        'content': 'lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+        'date_posted':'September 22,2020'
+    }
+]
+
+@app.route('/')
+def home():
+    return render_template('home.html', posts = posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'About')
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created for {}'.format(form.username.data), category = 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title = 'Register', form= form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form = form)
+
+if __name__ == "__main__":
+    app.run(debug = True)
