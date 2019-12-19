92f95769a387975765fabb000b179a2375b3a501
hashbanger <prashant.brahmbhatt32@outlook.com>
2019-12-20 00:29:57 +0530

    updated the login procedure

--- Using_Flask/app.py
+++ Using_Flask/app.py
@@ -33,9 +33,15 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title = 'Register', form= form)
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'prashant.brahmbhatt32@outlook.com' and form.password.data == 'password':
+            flash('You have successfully logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username/password', 'danger')
     return render_template('login.html', title = 'Login', form = form)
 
 if __name__ == "__main__":
