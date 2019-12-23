9297928899eb7e90950e302af3b287df88ef2ae2
hashbanger <prashant.brahmbhatt32@outlook.com>
2019-12-23 18:02:11 +0530

    Added user posts feature

--- Using_Flask/flaskblog/routes.py
+++ Using_Flask/flaskblog/routes.py
@@ -134,8 +134,11 @@ def delete_post(post_id):
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
 
-@app.route('/')
-def home():
+@app.route('/user/<string:username>')
+def user_posts(username):
     page = request.args.get('page', default = 1, type = int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page = 5)
-    return render_template('home.html', posts = posts)
+    user = User.query.filter_by(username = username).first_or_404()
+    posts = Post.query.filter_by(author = user)\
+            .order_by(Post.date_posted.desc())\
+            .paginate(per_page = 5)
+    return render_template('user_posts.html', posts = posts, user = user)
